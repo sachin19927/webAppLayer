c3e1c10b6789f1ab60ddb0802d2463a9dd08d77a
sachin19927 <sachin19927@gmail.com>
2023-03-16 04:33:11 +0530

    fix commit

--- src/app/service/receipe-service.service.ts
+++ src/app/service/receipe-service.service.ts
@@ -1,38 +1,22 @@
 import { Injectable } from '@angular/core';
 import { ENDPOINTS } from '@app/endpoints';
-<<<<<<< HEAD
 import { Observable } from 'rxjs/internal/Observable';
 import { Receipe } from '../entity/receipe';
-=======
-import { Observable } from 'rxjs';
-import { Book } from '../entity/book';
->>>>>>> d64a5c7980de1dc30f736f9775adb36c4336bbf5
 import { HttpClientService } from './http-client.service';
 
 @Injectable({
   providedIn: 'root'
   
 })
-<<<<<<< HEAD
 export class ReceipeServiceService extends HttpClientService{
 
   URL=ENDPOINTS.RECEIPE_DATA ;
   public getResults(url:string,params:any):Observable<Receipe[]>
-=======
-export class ReceipeServiceService extends HttpClientService {
-
-  URL=ENDPOINTS.LIBRARY_DATA;
-  public getResults(url:string,params:any):Observable<Book[]>
->>>>>>> d64a5c7980de1dc30f736f9775adb36c4336bbf5
   {
     return this.getAll(this.URL,params)
   }
 
-<<<<<<< HEAD
   public getResultId(url:string,params:any):Observable<Receipe>
-=======
-  public getResultId(url:string,params:any):Observable<Book>
->>>>>>> d64a5c7980de1dc30f736f9775adb36c4336bbf5
   {
     return this.getById(this.URL,params)
   }
@@ -42,11 +26,7 @@ export class ReceipeServiceService extends HttpClientService {
     return this.create(this.URL,data)
   }
 
-<<<<<<< HEAD
   public updateData(url:string,params:any,data:any):Observable<Receipe>
-=======
-  public updateData(url:string,params:any,data:any):Observable<Book>
->>>>>>> d64a5c7980de1dc30f736f9775adb36c4336bbf5
   {
     return this.update(this.URL,params,data)
   }
