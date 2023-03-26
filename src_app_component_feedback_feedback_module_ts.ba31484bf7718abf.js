170aaff3fe83a28e25741458d2d6f1ad236d14dd
sachin19927 <sachin19927@gmail.com>
2023-03-26 20:02:33 +0000

    Deploying to gh-pages from  @ ae650bafdb8fe6935f2edf480105dc8f1f5b82ed 🚀

---
+++ src_app_component_feedback_feedback_module_ts.ba31484bf7718abf.js
@@ -0,0 +1,173 @@
+"use strict";
+(self["webpackChunkwebAppLayer"] = self["webpackChunkwebAppLayer"] || []).push([["src_app_component_feedback_feedback_module_ts"],{
+
+/***/ 4685:
+/*!*************************************************************************!*\
+  !*** ./src/app/component/feedback/feebackpage/feebackpage.component.ts ***!
+  \*************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "FeebackpageComponent": () => (/* binding */ FeebackpageComponent)
+/* harmony export */ });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ 4650);
+
+class FeebackpageComponent {}
+FeebackpageComponent.ɵfac = function FeebackpageComponent_Factory(t) {
+  return new (t || FeebackpageComponent)();
+};
+FeebackpageComponent.ɵcmp = /*@__PURE__*/_angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineComponent"]({
+  type: FeebackpageComponent,
+  selectors: [["app-feebackpage"]],
+  decls: 2,
+  vars: 0,
+  template: function FeebackpageComponent_Template(rf, ctx) {
+    if (rf & 1) {
+      _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](0, "p");
+      _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtext"](1, "feebackpage works!");
+      _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
+    }
+  }
+});
+
+/***/ }),
+
+/***/ 4944:
+/*!*******************************************************!*\
+  !*** ./src/app/component/feedback/feedback.module.ts ***!
+  \*******************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "FeedbackModule": () => (/* binding */ FeedbackModule)
+/* harmony export */ });
+/* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @angular/common */ 6895);
+/* harmony import */ var _feebackpage_feebackpage_component__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./feebackpage/feebackpage.component */ 4685);
+/* harmony import */ var _personal_details_personal_details_component__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./personal-details/personal-details.component */ 9167);
+/* harmony import */ var _query_page_query_page_component__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./query-page/query-page.component */ 886);
+/* harmony import */ var _preview_page_preview_page_component__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./preview-page/preview-page.component */ 309);
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @angular/core */ 4650);
+
+
+
+
+
+
+class FeedbackModule {}
+FeedbackModule.ɵfac = function FeedbackModule_Factory(t) {
+  return new (t || FeedbackModule)();
+};
+FeedbackModule.ɵmod = /*@__PURE__*/_angular_core__WEBPACK_IMPORTED_MODULE_4__["ɵɵdefineNgModule"]({
+  type: FeedbackModule
+});
+FeedbackModule.ɵinj = /*@__PURE__*/_angular_core__WEBPACK_IMPORTED_MODULE_4__["ɵɵdefineInjector"]({
+  imports: [_angular_common__WEBPACK_IMPORTED_MODULE_5__.CommonModule]
+});
+(function () {
+  (typeof ngJitMode === "undefined" || ngJitMode) && _angular_core__WEBPACK_IMPORTED_MODULE_4__["ɵɵsetNgModuleScope"](FeedbackModule, {
+    declarations: [_feebackpage_feebackpage_component__WEBPACK_IMPORTED_MODULE_0__.FeebackpageComponent, _personal_details_personal_details_component__WEBPACK_IMPORTED_MODULE_1__.PersonalDetailsComponent, _query_page_query_page_component__WEBPACK_IMPORTED_MODULE_2__.QueryPageComponent, _preview_page_preview_page_component__WEBPACK_IMPORTED_MODULE_3__.PreviewPageComponent],
+    imports: [_angular_common__WEBPACK_IMPORTED_MODULE_5__.CommonModule]
+  });
+})();
+
+/***/ }),
+
+/***/ 9167:
+/*!***********************************************************************************!*\
+  !*** ./src/app/component/feedback/personal-details/personal-details.component.ts ***!
+  \***********************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "PersonalDetailsComponent": () => (/* binding */ PersonalDetailsComponent)
+/* harmony export */ });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ 4650);
+
+class PersonalDetailsComponent {}
+PersonalDetailsComponent.ɵfac = function PersonalDetailsComponent_Factory(t) {
+  return new (t || PersonalDetailsComponent)();
+};
+PersonalDetailsComponent.ɵcmp = /*@__PURE__*/_angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineComponent"]({
+  type: PersonalDetailsComponent,
+  selectors: [["app-personal-details"]],
+  decls: 2,
+  vars: 0,
+  template: function PersonalDetailsComponent_Template(rf, ctx) {
+    if (rf & 1) {
+      _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](0, "p");
+      _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtext"](1, "personal-details works!");
+      _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
+    }
+  }
+});
+
+/***/ }),
+
+/***/ 309:
+/*!***************************************************************************!*\
+  !*** ./src/app/component/feedback/preview-page/preview-page.component.ts ***!
+  \***************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "PreviewPageComponent": () => (/* binding */ PreviewPageComponent)
+/* harmony export */ });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ 4650);
+
+class PreviewPageComponent {}
+PreviewPageComponent.ɵfac = function PreviewPageComponent_Factory(t) {
+  return new (t || PreviewPageComponent)();
+};
+PreviewPageComponent.ɵcmp = /*@__PURE__*/_angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineComponent"]({
+  type: PreviewPageComponent,
+  selectors: [["app-preview-page"]],
+  decls: 2,
+  vars: 0,
+  template: function PreviewPageComponent_Template(rf, ctx) {
+    if (rf & 1) {
+      _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](0, "p");
+      _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtext"](1, "preview-page works!");
+      _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
+    }
+  }
+});
+
+/***/ }),
+
+/***/ 886:
+/*!***********************************************************************!*\
+  !*** ./src/app/component/feedback/query-page/query-page.component.ts ***!
+  \***********************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "QueryPageComponent": () => (/* binding */ QueryPageComponent)
+/* harmony export */ });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ 4650);
+
+class QueryPageComponent {}
+QueryPageComponent.ɵfac = function QueryPageComponent_Factory(t) {
+  return new (t || QueryPageComponent)();
+};
+QueryPageComponent.ɵcmp = /*@__PURE__*/_angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineComponent"]({
+  type: QueryPageComponent,
+  selectors: [["app-query-page"]],
+  decls: 2,
+  vars: 0,
+  template: function QueryPageComponent_Template(rf, ctx) {
+    if (rf & 1) {
+      _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](0, "p");
+      _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtext"](1, "query-page works!");
+      _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
+    }
+  }
+});
+
+/***/ })
+
+}]);
